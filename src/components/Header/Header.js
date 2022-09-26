07e62f5c7353de66c502952422d6fbfe417e08ac
zahidhasan125 <zahidhasan125@gmail.com>
2022-09-26 00:51:50 -0400

    navbar added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './Header.css'
+import logo from '../../images/Logo.svg'
 
 const Header = () => {
     return (
-        <div>
-            <h1>Hi</h1>
-        </div>
+        <nav className='navbar'>
+            <img src={logo} alt="logo" />
+            <div className='menu'>
+                <a href="/shop">Shop</a>
+                <a href="/order">Order</a>
+                <a href="/inventory">Manage Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </nav>
     );
 };
 
